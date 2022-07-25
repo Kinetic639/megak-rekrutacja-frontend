e6fbf68c995fadd2203a39d2cea328b1f11ff12d
Artur Masłowski <bolo0010.pl@gmail.com>
2022-07-25 21:57:50 +0200

    Added variables in index.css.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import { UserLoginSite } from './views/UserLoginSite';
 import { AvailableStudentsSite } from './views/AvailableStudentsSite';
+import { Admin } from './commponents/Admin/Admin';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           path={'/available-students'}
           element={<AvailableStudentsSite />}
         />
+        <Route path={'/admin'} element={<Admin />} />
       </Routes>
     </>
   );
