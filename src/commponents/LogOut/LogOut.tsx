4195d58b16a455878844bca47e55aa1325d8702d
Karolina Sylwia Banach <karolina.sylwia.banach@gmail.com>
2022-07-30 10:24:24 +0200

    successful logout

--- src/commponents/LogOut/LogOut.tsx
+++ src/commponents/LogOut/LogOut.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import '../../button.css'
-import{Router} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 export const LogOut = () => {
+    const nav = useNavigate();
 
-            const logOut = async () => {
-                await fetch('http://localhost:3001/auth/logout');
+    const logOut = async () => {
+        const res = await fetch('http://localhost:3001/auth/logout',{
+            method: 'GET',
+        });
+        const resJson = await res.json()
+        if(resJson.message === 'Logout successful'){
+        nav("/auth/login");
+        }
+        console.log("log out");
+    };
 
-            };
-
-        return <>
-            <button
+    return <>
+        <button
             className="button"
-            onClick={() => logOut}
+            onClick={logOut}
         >
             Wyloguj
         </button>
 
 
-        </>;
-    };
+    </>;
+};
