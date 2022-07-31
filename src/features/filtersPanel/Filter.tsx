3ac50ec3e098bdf57a05b15d03258a4b32154434
Lukasz Pelczar <lukasxxz@o2.pl>
2022-07-31 10:40:43 +0200

    remove empty css file

--- src/features/filtersPanel/Filter.tsx
+++ src/features/filtersPanel/Filter.tsx
@@ -1,27 +1,25 @@
-import './Filter.css';
-
-interface FilterI{
-    subTitle:string,
-    listComponents:JSX.Element[],
-    classes?:string
+interface FilterI {
+    subTitle: string,
+    listComponents: JSX.Element[],
+    classes?: string
 }
 
-export const Filter: React.FC<FilterI> =({subTitle, listComponents,classes=''})=>{
+export const Filter: React.FC<FilterI> = ({ subTitle, listComponents, classes = '' }) => {
 
-    const listContent= listComponents.map((item,index)=>{
+    const listContent = listComponents.map((item, index) => {
         return (
             <li key={`${subTitle}${index}`}>{item}</li>
         )
     })
 
-    return(
+    return (
         <>
-        <section className="filtersPanel__filter">
-            <h3 className="filtersPanel__subtitle">{subTitle}</h3>
-            <ul className={`filtersPanel__options-list ${classes}`}>
-                {listContent}
-            </ul>
-          </section>
+            <section className="filtersPanel__filter">
+                <h3 className="filtersPanel__subtitle">{subTitle}</h3>
+                <ul className={`filtersPanel__options-list ${classes}`}>
+                    {listContent}
+                </ul>
+            </section>
         </>
     )
 }
