0efa6fa5b8870e99c17be61b303a35c75de32486
Lukasz Pelczar <lukasxxz@o2.pl>
2022-08-01 21:07:28 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { FiltersPanel } from './features/filtersPanel/FiltersPanel';
 import './App.css';
 
 function App() {
   return (
     <>
-      <FiltersPanel closeCallback={() => { }} />
     </>
   );
 }
