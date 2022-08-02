2c906ea12d91a136cafcdb11ba718ea5c9fddff3
FrostKiller <bialywilk50@gmail.com>
2022-08-02 21:11:39 +0200

    Change table with information to full scalable.

--- src/commponents/AvailableStudents/AvailableStudents.tsx
+++ src/commponents/AvailableStudents/AvailableStudents.tsx
@@ -72,52 +72,144 @@ const AvailableStudents = () => {
               </Button>
             </Accordion.Header>
             <Accordion.Body>
-              <Container fluid>
-                <Row className={'p-3 row-main row-cols-8 accordion-body-color'}>
-                  <Col className={`accordion-body-color`}>
-                    Ocena przejścia kursu
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Ocena aktywności i zaangażowania na kursie
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Ocena kodu w projekcie własnym
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Ocena pracy w zespole Scrum
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Preferowane miejsce pracy
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Docelowe miasto, gdzie chce pracować kandydat
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Oczekiwant typ kontraktu
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    Oczekiwane wynagrodzenie miesięczne netto
-                  </Col>
-                </Row>
-                <Row className={'p-3 row-second accordion-body-color'}>
-                  <Col className={`accordion-body-color`}>
-                    <span className={`row-second-span`}>5</span>/5
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    <span className={`row-second-span`}>4</span>/5
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    <span className={`row-second-span`}>3</span>/5
-                  </Col>
-                  <Col className={`accordion-body-color`}>
-                    <span className={`row-second-span`}>2</span>/5
-                  </Col>
-                  <Col className={`accordion-body-color`}>Biuro</Col>
-                  <Col className={`accordion-body-color`}>Warszawa</Col>
-                  <Col className={`accordion-body-color`}>Ummowa o pracę</Col>
-                  <Col className={`accordion-body-color`}>8 000 zł</Col>
-                </Row>
-              </Container>
+              <CardGroup>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Ocena przejścia kursu
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>
+                      5<span className={`row-second-span`}>/5</span>
+                    </Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Ocena aktywności i zaangażowania na kursie
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>
+                      5<span className={`row-second-span`}>/4</span>
+                    </Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Ocena kodu w projekcie własnym
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>
+                      5<span className={`row-second-span`}>/3</span>
+                    </Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Ocena pracy w zespole Scrum
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>
+                      5<span className={`row-second-span`}>/2</span>
+                    </Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Preferowane miejsce pracy
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>Biuro</Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Docelowe miasto, gdzie chce pracować kandydat
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>Warszawa</Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Oczekiwant typ kontraktu
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>Ummowa o pracę</Col>
+                  </Row>
+                </Card>
+                <Card>
+                  <Row
+                    className={
+                      'pt-3 pb-1 ps-1 pe-1 row-main accordion-body-color'
+                    }
+                  >
+                    <Col className={`accordion-body-color`}>
+                      Oczekiwane wynagrodzenie miesięczne netto
+                    </Col>
+                  </Row>
+                  <Row
+                    className={'pb-3 ps-1 pe-1 row-second accordion-body-color'}
+                  >
+                    <Col className={`accordion-body-color`}>8 000 zł</Col>
+                  </Row>
+                </Card>
+              </CardGroup>
             </Accordion.Body>
             <p />
           </Accordion.Item>
