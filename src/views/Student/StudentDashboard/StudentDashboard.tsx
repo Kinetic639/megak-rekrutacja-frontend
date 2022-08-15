c0aa7951c569440a33da941edd8a7349937f47d8
hubertkucharski <hubertkucharski@gmail.com>
2022-08-15 14:16:37 +0200

    added student condition for first logging (if all necessary information not filled)

--- src/views/Student/StudentDashboard/StudentDashboard.tsx
+++ src/views/Student/StudentDashboard/StudentDashboard.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import { StudentCV } from '../StudenctCV/StudenctCV';
 import { useAppSelector } from '../../../redux/hooks/hooks';
 import { CustomSpinner } from '../../../components/common/CustomSpinner/CustomSpinner';
+import { StudentForm } from '../StudentForm/StudentForm';
 
 export const StudentDashboard = () => {
   const currUser = useAppSelector((state) => state.user.user);
   if (!currUser) {
     return <CustomSpinner />;
   }
-  return <StudentCV />;
+  const {
+    tel,
+    githubUsername,
+    teamProjectUrls,
+    canTakeApprenticeship,
+    monthsOfCommercialExp,
+  } = currUser;
+
+  if (
+    tel ||
+    githubUsername ||
+    teamProjectUrls ||
+    canTakeApprenticeship ||
+    monthsOfCommercialExp
+  )
+    return <StudentCV />;
+  else return <StudentForm />;
 };
